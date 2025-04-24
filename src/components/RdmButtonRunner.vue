cb42813d98cb98ed718c53b423057c5c7024911c
RDayalMunda <r.dayal.munda@gmail.com>
2025-04-24 20:32:00 +0530

    button themes

--- src/components/RdmButtonRunner.vue
+++ src/components/RdmButtonRunner.vue
@@ -5,8 +5,15 @@
     <button @click="handleClick">A simple button with a long text</button>
   </div>
   <hr />
-  <div>
-    <RdmButton text="Click Me" @click="handleClick" />
+  <div class="custom-container">
+    <div>
+      <label>Primary Button</label>
+      <RdmButton text="Click Me" @click="handleClick" />
+    </div>
+    <div>
+      <label>Secondary Button</label>
+      <RdmButton text="Click Me" variant="secondary" @click="handleClick" />
+    </div>
   </div>
 </template>
 
@@ -17,3 +24,13 @@ function handleClick() {
   console.log("clicked");
 }
 </script>
+<style scoped>
+.custom-container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.custom-container label {
+  margin: 10px;
+}
+</style>
